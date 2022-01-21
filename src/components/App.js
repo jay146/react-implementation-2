59acae32bc35dc6168178eb39e85da9936b2eb4e
Jayakrishna Surampudi <Jayakrishna.jk14@gmail.com>
2022-01-21 14:27:08 +0530

    added key for displaying notes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Note from "./Note";
+import notes from "../notes";
+import Notes from "./Note";
+console.log(notes);
 
+function createNotes(noteitem) {
+  return (
+    <Notes
+      key={noteitem.key}
+      title={noteitem.title}
+      content={noteitem.content}
+    />
+  );
+}
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {/* <Note title="hi jay" content="my content" /> */}
+      {notes.map(createNotes)}
       <Footer />
     </div>
   );
